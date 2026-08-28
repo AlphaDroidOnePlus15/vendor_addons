07f9cc52daa639e527c94c78e6739db934e5695e
RedLintu16 <bearblanket@gmail.com>
2026-08-27 22:35:51 -0700

    Remove things I don't want and add Raleway.

--- config.mk
+++ config.mk
@@ -15,206 +15,21 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Prebuilt apks
-ifeq ($(TARGET_INCLUDE_SIMPLE_TUNE),true)
-  PRODUCT_PACKAGES += \
-    ViviMusic
-endif
-
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
     UdfpsIcons \
     UdfpsAnimations
 endif
 
-ifneq ($(TARGET_BUILD_PACKAGE),3)
-PRODUCT_COPY_FILES += \
-    vendor/addons/prebuilt/product/etc/sysconfig/ambient_data.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/ambient_data.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/contextual_search.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/contextual_search.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/dialer_experience.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google_build.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_build.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google-initial-package-stopped-states.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-initial-package-stopped-states.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/nexus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/nexus.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/nga.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/nga.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/pixel_2017-initial-package-stopped-states.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2017-initial-package-stopped-states.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2017-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2017-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2022-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2022-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2023-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2023-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2024-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2024-and-newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled_packages_pixel_experience_2017_and_newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled_packages_pixel_experience_2017_and_newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled_packages_pixel_experience_2021_and_newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled_packages_pixel_experience_2021_and_newer.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/preinstalled_packages_repair_mode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled_packages_repair_mode.xml
-endif
-
-# Clocks
-PRODUCT_PACKAGES += \
-    SystemUIClocks-BigNum \
-    SystemUIClocks-Calligraphy \
-    SystemUIClocks-Growth \
-    SystemUIClocks-Inflate \
-    SystemUIClocks-Metro \
-    SystemUIClocks-NumOverlap \
-    SystemUIClocks-Weather
-
 # Custom Overlays
 PRODUCT_PACKAGES += \
-    GlanceableHubConfigOverlay \
-    GlanceableHubSettingsConfigOverlay \
-    GlanceableHubSettingsConfigOverlay2022 \
-    GlanceableHubSysuiConfigOverlay \
     PixelLauncherOverlayCustom
 
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
-    ClockFont3DIsometricBlackOverlay \
-    ClockFont3DIsometricBoldOverlay \
-    ClockFontACFilmstripOverlay \
-    ClockFontAccuratistOverlay \
-    ClockFontAclonicaOverlay \
-    ClockFontAlmonteSnowOverlay \
-    ClockFontAlphaCloudsOverlay \
-    ClockFontAlphaFlowersOverlay \
-    ClockFontAlphaWoodOverlay \
-    ClockFontAmaranteOverlay \
-    ClockFontAmpad3D2Overlay \
-    ClockFontBalticBoddenOverlay \
-    ClockFontBalticCoastOverlay \
-    ClockFontBalticDuneOverlay \
-    ClockFontBalticStormOverlay \
-    ClockFontBariolOverlay \
-    ClockFontBetsyFlanaganOverlay \
-    ClockFontBigCheeseOverlay \
-    ClockFontBrandayolqOverlay \
-    ClockFontBudmoJigglerOverlay \
-    ClockFontBunnyRabbitsOverlay \
-    ClockFontCFBadNewsOverlay \
-    ClockFontCFOneTwoTreesOverlay \
-    ClockFontCafe24DecoshadowOverlay \
-    ClockFontCagliostroOverlay \
-    ClockFontCatOverlay \
-    ClockFontCoconOverlay \
-    ClockFontComfortaaOverlay \
-    ClockFontComicSansOverlay \
-    ClockFontConcentrateOverlay \
-    ClockFontCookieRunOverlay \
-    ClockFontCoolstoryOverlay \
-    ClockFontCrackmanOverlay \
-    ClockFontDiscoMidnightOverlay \
-    ClockFontEasterBunnyOverlay \
-    ClockFontEditPointsFilledOverlay \
-    ClockFontEditPointsOverlay \
-    ClockFontElriott2Overlay \
-    ClockFontExotwoOverlay \
-    ClockFontFibographyOverlay \
-    ClockFontFifa2018Overlay \
-    ClockFontFloorlightOverlay \
-    ClockFontFortaOverlay \
-    ClockFontGautsMotelUpperRightOverlay \
-    ClockFontGoogleSansFlexOverlay \
-    ClockFontGrandHotelOverlay \
-    ClockFontHangedOverlay \
-    ClockFontHarmonySansOverlay \
-    ClockFontHotSweatOverlay \
-    ClockFontKGOnlyHopeOverlay \
-    ClockFontKaramuruhOverlay \
-    ClockFontKingthingsOverlay \
-    ClockFontLMSCliffordOverlay \
-    ClockFontLatoOverlay \
-    ClockFontLinotteOverlay \
-    ClockFontLittleBunnyOverlay \
-    ClockFontLowerAtmosphereOverlay \
-    ClockFontMXWasgardOverlay \
-    ClockFontMessingLetternOverlay \
-    ClockFontMonbijouxClownpieceOverlay \
-    ClockFontMotorola \
-    ClockFontMuseoModernoOverlay \
-    ClockFontNeonDiscoOverlay \
-    ClockFontNeptunCATOverlay \
-    ClockFontNinjasOverlay \
-    ClockFontNokiaPureOverlay \
-    ClockFontNothingDotHeadlineOverlay \
-    ClockFontNunitoOverlay \
-    ClockFontOneplusSansOverlay \
-    ClockFontOneplusSlateOverlay \
-    ClockFontOswaldOverlay \
-    ClockFontPinewoodOverlay \
-    ClockFontPlaidEventOverlay \
-    ClockFontPlantsLettersOverlay \
-    ClockFontPlayOverlay \
-    ClockFontProdeltCoOverlay \
-    ClockFontQuandoOverlay \
-    ClockFontQuickSouthOverlay \
-    ClockFontRedressedOverlay \
-    ClockFontReemKufiOverlay \
-    ClockFontRemponkOverlay \
-    ClockFontRobotoCondensedOverlay \
-    ClockFontRomantiquesOverlay \
-    ClockFontRoundheadsOverlay \
-    ClockFontRubikGlitchOverlay \
-    ClockFontRubikOverlay \
-    ClockFontSamsungOneOverlay \
-    ClockFontSansSerifOverlay \
-    ClockFontScrapItUpOverlay \
-    ClockFontSonySketchOverlay \
-    ClockFontSpaceGameOverlay \
-    ClockFontStandardHeaderOverlay \
-    ClockFontStoropiaOverlay \
-    ClockFontSurferOverlay \
-    ClockFontTh3machineOverlay \
-    ClockFontTourneyMediumOverlay \
-    ClockFontUbuntuOverlay \
-    ClockFontVtksdura3dOverlay \
-    ClockFontZnikomitNo24Overlay \
-    ClockFontIOSOverlay \
-    ClockFontHerculesOverlay \
-    ClockFontSlimOverlay \
-    ClockFontNtype82Overlay \
-    ClockFontSubwayOverlay \
-    FontAccuratistOverlay \
-    FontAclonicaOverlay \
-    FontAmaranteOverlay \
-    FontBariolOverlay \
-    FontCagliostroOverlay \
-    FontCoconOverlay \
-    FontComfortaaOverlay \
-    FontComicSansOverlay \
-    FontCookieRunOverlay \
-    FontCoolstoryOverlay \
-    FontExotwoOverlay \
-    FontFifa2018Overlay \
-    FontGrandHotelOverlay \
-    FontGoogleSansFlexOverlay \
-    FontHarmonySansOverlay \
-    FontIBMPlexSansOverlay \
-    FontLatoOverlay \
-    FontLinotteOverlay \
-    FontNokiaPureOverlay \
-    FontNothingDotHeadlineOverlay \
-    FontNothingDotOverlay \
-    FontNunitoOverlay \
-    FontOneplusSansOverlay \
-    FontOneplusSlateOverlay \
-    FontOswaldOverlay \
-    FontPlayOverlay \
-    FontQuandoOverlay \
-    FontRedressedOverlay \
-    FontReemKufiOverlay \
-    FontRobotoCondensedOverlay \
-    FontRookeryOverlay \
-    FontRubikOverlay \
-    FontSanFranciscoDisplayProSourceOverlay \
-    FontSamsungOneOverlay \
-    FontSansSerifOverlay \
-    FontSonySketchOverlay \
-    FontStoropiaOverlay \
-    FontSurferOverlay \
-    FontUbuntuOverlay \
-    FontExtFontOverlay
+    FontRalewayOverlay\
+    ClockFontRalewayOverlay
 
 # Navbar styles
 PRODUCT_PACKAGES += \
@@ -305,23 +120,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
     BackGestureDotTrailOverlay \
-    NothingChargingAnimationOverlay \
-    MotoChargingAnimationOverlay \
-    BatteryStyleiOS15Overlay \
-    BatteryStyleiOS16Overlay \
-    BatteryStyleOneUI7Overlay \
-    BatteryStyleOrigamiOverlay \
     BatteryStyleSmileyOverlay
-
-# Basic call recorder
-ifeq ($(strip $(TARGET_PREBUILT_BCR)),true)
-PRODUCT_PACKAGES += \
-    Bcr
-endif
-
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
